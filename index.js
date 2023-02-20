e0e27bdd914b2806bb482c957d996fd3790ecc6b
Sharmi1994 <psharmipsna@gmail.com>
2023-02-20 14:46:21 -0500

    add collapsible div

---
+++ index.js
@@ -0,0 +1,19 @@
+function toggle(){
+  var div= document.getElementsByClassName("Divide");
+  var icon=document.querySelector("i");
+ 
+  Array.prototype.forEach.call(div, function(element, index) {
+    if (elemen.style.display === "none") {
+        element.style.display = "block";
+      } else {
+        element.style.display = "none";
+      }
+    }
+  );
+    
+
+    }
+
+
+
+

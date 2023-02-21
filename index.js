1d54b09d17c04b58ad47ffee083efd60ae3747fc
Sharmi1994 <psharmipsna@gmail.com>
2023-02-21 15:05:02 -0500

    Netflix last section added

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-function toggle(){
-  var div= document.getElementsByClassName("Divide");
-  var icon=document.querySelector("i");
- 
-  Array.prototype.forEach.call(div, function(element, index) {
-    if (elemen.style.display === "none") {
+function toggle(togleIndex) {
+  var div = document.getElementsByClassName("Divide");
+
+  Array.prototype.forEach.call(div, function (element, index) {
+    console.log(element);
+    if (index === togleIndex) {
+      if (element.style.display === "none") {
         element.style.display = "block";
       } else {
         element.style.display = "none";
       }
     }
-  );
-    
-
-    }
-
-
-
-
+  });
+}

ffb366a419ae7f7aa1d1759eb00d9081cf7f896b
Sharmi1994 <psharmipsna@gmail.com>
2023-02-23 07:10:19 -0500

    netflix java script completion

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 function toggle(togleIndex) {
   var div = document.getElementsByClassName("Divide");
 
-  Array.prototype.forEach.call(div, function (element, index) {
-    console.log(element);
+  var div2 = document.getElementsByClassName("myDiv");
+
+  Array.prototype.forEach.call(div2, function (e, index) {
     if (index === togleIndex) {
+      // chnage icon
+      e.getElementsByTagName("i")[0].classList.toggle("fa-minus");
+    }
+  });
+
+  Array.prototype.forEach.call(div, function (element, index) {
+    if (index === togleIndex) {
+      // chnage icon
+      console.log(element);
       if (element.style.display === "none") {
         element.style.display = "block";
       } else {
